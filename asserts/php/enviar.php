38d9618e0ea42b6407ce411ec31a9bb37e9b0a61
Dgley Ramos <dgleysilva@gmail.com>
2022-01-10 16:37:23 -0300

    modificando o footer

--- asserts/php/enviar.php
+++ asserts/php/enviar.php
@@ -1,50 +1,103 @@
 <?php
 
-if (isset($_POST['BTEnvia'])) {
+##---------------------------------------------------
+  ##  Envio de Emails pelo SMTP Aut�nticado usando PEAR
+  ##---------------------------------------------------
+  # Mais detalhes sobre o PEAR: 
+  #   http://pear.php.net/
+  #
+  # Mais detalhes sobre o PEAR Mail:
+  #   http://pear.php.net/manual/en/package.mail.mail-mime.php
+  ##---------------------------------------------------
+  
+  # Faz o include do PEAR Mail e do Mime.
+  include ("Mail.php");
+  include ("Mail/mime.php");
+  
+  //Variaveis de POST, Alterar somente se necessário 
+//===================================================
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['mensagem'];
+//====================================================
 
-    //Variaveis de POST, Alterar somente se necessário 
-    //====================================================
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $assunto = $_POST['assunto'];
-    $mensagem = $_POST['mensagem'];
-    //====================================================
+//REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
+//==================================================== 
+$email_remetente = "site@allianzcontabilidadecr.com"; // deve ser uma conta de email do seu dominio 
+//====================================================
 
-    //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
-    //==================================================== 
-    $email_remetente = "site@allianzcontabilidadecr.com"; // deve ser uma conta de email do seu dominio 
-    //====================================================
+//Configurações do email, ajustar conforme necessidade
+//==================================================== 
+$email_reply = "$email"; 
+//====================================================
+  # E-mail de destino. Caso seja mais de um destino, crie um array de e-mails.
+  # *OBRIGAT�RIO*
+  $recipients = 'dgleysilva@gmail.com';
 
-    //Configurações do email, ajustar conforme necessidade
-    //==================================================== 
-    $email_destinatario = "dgleysilva@gmail.com"; // pode ser qualquer email que receberá as mensagens
-    $email_reply = "$email"; 
-    $email_assunto = "$assunto"; // Este será o assunto da mensagem
-    //====================================================
+  # Cabe�alho do e-mail.
+  $headers = 
+    array (
+      'From'    => 'site@allianzcontabilidadecr.com', # O 'From' � *OBRIGAT�RIO*.
+      'To'      => $recipients,
+      'Subject' => 'Mensagem enviada do site'
+    );
 
-    //Monta o Corpo da Mensagem
-    //====================================================
-    $email_conteudo = "Nome: ".$nome. "\n"; 
-    $email_conteudo .= "Email: ".$email. "\n"; 
-    $email_conteudo .= "Mensagem: ".$mensagem. "\n"; 
-    //====================================================
+  # Utilize esta op��o caso deseje definir o e-mail de resposta
+  $headers['Reply-To'] = "$email_reply";
 
-    //Seta os Headers (Alterar somente caso necessario) 
-    //==================================================== 
-    $header = "From: $email_remetente".
-              "Reply-To: ".$email_reply." \r\n".
-              "Return-Path: ".$email_remetente." \r\n".
-              "X=Mailer:PHP/".phpversion();
-    //====================================================
+  # Utilize esta op��o caso deseje definir o e-mail de retorno em caso de erro de envio
+  # $headers['Errors-To'] = 'EMailDeRerornoDeERRO@DominioDeretornoDeErro.com';
 
-    //Enviando o email 
-    //==================================================== 
-    if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $header)){ 
-        echo "<script>document.location = 'https://allianz-contabilidade.vercel.app/'</script>"; 
-    } 
-    else{ 
-        echo "<script>document.location = 'https://allianz-contabilidade.vercel.app/'</script>";
-    } 
-    //====================================================
-} 
+  # Utilize esta op��o caso deseje definir a prioridade do e-mail
+  # $headers['X-Priority'] = '3'; # 1 UrgentMessage, 3 Normal  
+
+  # Define o tipo de final de linha.
+  $crlf = "\r\n";
+
+  # Corpo da Mensagem e texto e em HTML
+  $text = "Nome: ".$_POST['nome']." <br>";
+  $text .= "E-mail: ".$email. " <br>";
+  $text .= "Assunto: ".$assunto. "<br>";
+  $text .= "Mensagem: ".$mensagem. "";
+  $html = "<HTML><BODY><font color=black>$text</font></BODY></HTML>";
+
+
+  # Instancia a classe Mail_mime
+  $mime = new Mail_mime($crlf);
+
+  # Coloca o HTML no email
+  $mime->setHTMLBody($html);
+
+
+##  # Anexa um arquivo ao email.
+  
+  # Procesa todas as informa��es.
+  $body = $mime->get();
+  $headers = $mime->headers($headers);
+
+  # Par�metros para o SMTP. *OBRIGAT�RIO*
+  $params = 
+    array (
+      'auth' => true, # Define que o SMTP requer autentica��o.
+      'host' => 'smtp.allianzcontabilidadecr.com', # Servidor SMTP
+      'username' => 'site=allianzcontabilidadecr.com', # Usu�rio do SMTP
+      'password' => 'Allianz2022contabilidade' # Senha do seu MailBox.
+    );
+    
+  # Define o m�todo de envio
+  $mail = new Mail();
+  $mail_object = $mail->factory('smtp', $params);
+
+  # Envia o email. Se n�o ocorrer erro, retorna TRUE caso contr�rio, retorna um
+  # objeto PEAR_Error. Para ler a mensagem de erro, use o m�todo 'getMessage()'.
+  $result = $mail_object->send($recipients, $headers, $body);
+  if (PEAR::IsError($result))
+  {
+    echo "ERRO ao tentar enviar o email. (" . $result->getMessage(). ")";
+  }   
+  else
+  {
+    echo "Email enviado com sucesso!";
+       
+  }
 ?>
